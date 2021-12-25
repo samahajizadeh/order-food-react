be254615a898ec812c238fbefb99d3b877488a99
samaneh hajizadeh <samahajizadeh15@gmail.com>
2021-12-25 15:59:19 +0330

    change root home

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
           <Suspense fallback={<Preloader />}>
             <Routes>
               {/* <Route path="/" element={<LoginPage />} /> */}
-              <Route path="/order-food-react" element={<Navigate replace to="/"/>}/>
+              <Route path="/order-food-react/" element={<Navigate replace to="/home"/>}/>
               <Route path="/" element={<Navigate replace to="/home"/>}/>
               <Route path="/home" element={<HomePage />} />
               <Route path="/order" element={<OrderPage />} />
